d089fe2f292b14b9074d188951ba8375d749688f
t-katsumura <t-katsumura@nri.co.jp>
2025-05-13 15:29:20 +0900

    update
    Signed-off-by: t-katsumura <t-katsumura@nri.co.jp>

--- make.sh
+++ make.sh
@@ -1,18 +1,21 @@
 #! /bin/bash
 
+# Input variables.
 # PROJECT="aileron-test"
-git clone "https://github.com/aileron-project/${PROJECT}.git"
-TAGS=$(cd "${PROJECT}" && git tag)
 # PATTERNS=()
 
+# Initialize.
+git clone "https://github.com/aileron-project/${PROJECT}.git"
+TAGS=$(cd "${PROJECT}" && git tag)
+
+# Show all available tags.
 echo "--------------------"
 echo "TAGS"
 echo "${TAGS}"
 echo "--------------------"
 
+# Select target tags.
 TARGETS=()
-
-# echo "${TAGS}" | while read -r line; do
 while read -r line; do
   echo "TAG: ${line}"
   if [[ "${line}" =~ ^v[0-9]+.[0-9]+.[0-9]+$ ]]; then
@@ -28,13 +31,21 @@ while read -r line; do
   done
 done < <(echo "${TAGS}")
 
+# Show target tags.
 echo "TARGETS: ${TARGETS[*]}"
 
+# Generate documents from HEAD.
+PRJ="${PROJECT}"
+TAG="main"
+DATE=$(cd "${PROJECT}" && git log -1 --format=%cd --date=format:"%Y-%m-%d")
+make build PRJ="${PRJ}" TAG="${TAG}" DATE="${DATE}"
+
+# Generate documents from tags.
 for target in "${TARGETS[@]}"; do
   echo "Build document from ${PROJECT}:${target}"
-  _="$(cd "${PROJECT}" && git checkout "${target}")"
+  _=$(cd "${PROJECT}" && git checkout "${target}")
   PRJ="${PROJECT}"
   TAG="${target}"
-  DATE="$(cd "${PROJECT}" && git for-each-ref --format="%(taggerdate:short)" refs/tags/"${target}")"
+  DATE=$(cd "${PROJECT}" && git for-each-ref --format="%(taggerdate:short)" refs/tags/"${target}")
   make build PRJ="${PRJ}" TAG="${TAG}" DATE="${DATE}"
 done
